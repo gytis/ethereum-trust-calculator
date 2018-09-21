3264d831d3955b652b5c26c3942268336cd3db7d
Gytis Trikleris <gytis@redhat.com>
2018-09-22 01:04:08 +0200

    Standard application class

---
+++ src/main/java/gt/EthereumTrustCalculatorApplication.java
@@ -0,0 +1,13 @@
+package gt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EthereumTrustCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EthereumTrustCalculatorApplication.class, args);
+    }
+
+}
